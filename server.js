ee1de6b5dc0c568e9c62c6e9338abd3d2ab77747
Vayk <54449744+vaykmuniz@users.noreply.github.com>
2021-01-18 16:41:03 -0300

    middleware

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const connectDB = require('./database/db');
+const cors = require('cors');
+const morgan = require('morgan');
 
 const app = express();
 
+//middleware
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+//db
 connectDB();
+
+//routezzzzzz
+
+app.get('/', (req, res) => {
+    res.send("we r in guys");
+});
 
 const port = process.env.PORT || 5000;
 
