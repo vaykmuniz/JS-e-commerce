b830d81c23e4908067f424592ee0875823bfda48
Vayk <54449744+vaykmuniz@users.noreply.github.com>
2021-01-22 10:38:59 -0300

    Create User in DB

--- database/db.js
+++ database/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb://127.0.0.1/my_database';
+const url = 'mongodb://127.0.0.1:27017/my_database';
 
 const connectDB = async () => {
     try {
--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const { signupValidator, validatorResult } = require('../middleware/validator');
+const { signupController } = require('../controllers/auth');
 
 
 addHeader = function (req, res, next){
@@ -10,12 +11,6 @@ addHeader = function (req, res, next){
     next();
 };
 
-router.post('/signup', addHeader, signupValidator, validatorResult );
-
-/*
-router.post('/signup', (req, res)=>{
-    console.log('aaaaaa');
-});
-*/
+router.post('/signup', addHeader, signupValidator, validatorResult, signupController );
 
 module.exports = router;
